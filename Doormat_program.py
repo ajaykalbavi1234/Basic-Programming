bce472d70bc38948127bf391cb4e0cdbdea3fa95
ajaykalbavi1234 <43661371+ajaykalbavi123@users.noreply.github.com>
2021-02-18 23:17:33 +0530

    Update Doormat_program.py

--- Doormat_program.py
+++ Doormat_program.py
@@ -2,7 +2,7 @@ n = int(input())
 m = 3*n
 
 for i in range(1,n,2):
-    print(('.|.'*i).center(m,'-'))
+    print(('.||.'*i).center(m,'-'))
 print('WELCOME'.center(m,'-'))
 for i in range(n-2,-1,-2):
-    print(('.|.'*i).center(m,'-'))
+    print(('.||.'*i).center(m,'-'))
