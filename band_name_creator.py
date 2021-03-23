4ec536a23ea900dea31518dd8afaddbae537c581
ajaykalbavi1234 <43661371+ajaykalbavi123@users.noreply.github.com>
2021-03-23 11:48:17 +0530

    Create band_name_creator.py

---
+++ band_name_creator.py
@@ -0,0 +1,4 @@
+print('Hello, welcome to the band name generator')
+name1 = input('Enter the name of your hometown:')
+name2 = input('Enter the name of your pet or favorite animal name:')
+print('Your band name is:',name1+' '+name2)
